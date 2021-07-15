1229e5669216e18b368404600d33f4ce4a056165
Alberto Nascimento <beto.bdn@gmail.com>
2021-07-15 10:54:47 -0300

    Database seeding

---
+++ backend/client-api/src/main/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Ana', '987987777', 3500.0, NOW(), 2);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Maria', '785685685', 5150.5, NOW(), 1);
