423e08ebfbc9091f095f9063fa78ce45650a447a
Alberto Nascimento <beto.bdn@gmail.com>
2021-07-15 16:01:37 -0300

    Auditing date

--- backend/client-api/src/main/resources/data.sql
+++ backend/client-api/src/main/resources/data.sql
@@ -1,2 +1,2 @@
-INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Ana', '987987777', 3500.0, NOW(), 2);
-INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Maria', '785685685', 5150.5, NOW(), 1);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children, created_At) VALUES ('Ana', '987987777', 3500.0, TIMESTAMP WITH TIME ZONE '1999-01-20T10:30:00Z', 2, NOW());
+INSERT INTO tb_client (name, cpf, income, birth_Date, children, created_At) VALUES ('Juca', '785685685', 5150.5, TIMESTAMP WITH TIME ZONE '1989-02-20T10:30:00Z', 1, NOW());
