e1bb7b25e6bc6ca95af6a2b1015e1ffaf1b9e3ad
Alberto Nascimento <beto.bdn@gmail.com>
2021-07-27 19:43:48 -0300

    Add unit tests ClientServiceTests

--- backend/client-api/src/test/java/com/alberto/clientapi/services/ClientServiceTests.java
+++ backend/client-api/src/test/java/com/alberto/clientapi/services/ClientServiceTests.java
@@ -1,7 +1,13 @@
 package com.alberto.clientapi.services;
 
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
 import java.time.Instant;
 import java.util.List;
+import java.util.Optional;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -10,7 +16,6 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentMatchers;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
@@ -20,6 +25,7 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 import com.alberto.clientapi.dto.ClientDTO;
 import com.alberto.clientapi.entities.Client;
 import com.alberto.clientapi.repositories.ClientRepository;
+import com.alberto.clientapi.services.exceptions.ResourceNotFoundException;
 
 @ExtendWith(SpringExtension.class)
 public class ClientServiceTests {
@@ -29,11 +35,16 @@ public class ClientServiceTests {
 	
 	@Mock
 	private ClientRepository repository;
+	
+	private long existingId;
+	private long nonExistingId;
 	private Client client;
 	private PageImpl<Client> page;
 	
 	@BeforeEach
 	void setUp() {
+		existingId = 1L;
+		nonExistingId = 2L;
 		client = createClient();
 		page = new PageImpl<>(List.of(client));
 	}
@@ -43,12 +54,47 @@ public class ClientServiceTests {
 		
 		Pageable pageable = PageRequest.of(0, 6);
 		
-		Mockito.when(repository.findAll((Pageable) ArgumentMatchers.any())).thenReturn(page);
+		when(repository.findAll((Pageable) ArgumentMatchers.any())).thenReturn(page);
 		
 		Page<ClientDTO> result = service.findAllPaged(pageable);
 		
 		Assertions.assertNotNull(result);
-		Mockito.verify(repository, Mockito.times(1)).findAll(pageable);
+		verify(repository, times(1)).findAll(pageable);
+	}
+	
+	@Test
+	public void findByIdShouldReturnClientDTOWhenIdExist() {
+		
+		when(repository.findById(existingId)).thenReturn(Optional.of(client));
+		
+		ClientDTO clientDTO = service.findById(existingId);
+		
+		Assertions.assertNotNull(clientDTO);
+		verify(repository, times(1)).findById(existingId);
+	}
+	
+	@Test
+	public void findByIdShouldReturnThrowResourceNotFoundExceptionWhenIdDoesNotExist() {
+		
+		when(repository.findById(nonExistingId)).thenReturn(Optional.empty());
+		
+		Assertions.assertThrows(ResourceNotFoundException.class, () -> {
+			service.findById(nonExistingId);
+		});
+		
+		verify(repository, times(1)).findById(nonExistingId);
+	}
+	
+	@Test
+	public void deleteShouldDoNothingWhenIdExist() {
+		
+		doNothing().when(repository).deleteById(existingId);
+		
+		Assertions.assertDoesNotThrow(() -> {
+			service.delete(existingId);
+		});
+		
+		verify(repository, times(1)).deleteById(existingId);
 	}
 	
 	private Client createClient() {
